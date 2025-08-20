e0b230f9409553edd573db556fffa0de21f5aceb
Ahmad Mirza Rafiq Azmi <139027185+Amierza@users.noreply.github.com>
2025-08-20 13:28:34 +0700

    fix bug update ship images

--- entity/position_entity.go
+++ entity/position_entity.go
@@ -5,8 +5,9 @@ import (
 )
 
 type Position struct {
-	ID   uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
-	Name string    `gorm:"type:varchar(150);unique;not null" json:"name"`
+	ID     uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
+	IsTech bool      `json:"is_tech"`
+	Name   string    `gorm:"type:varchar(150);unique;not null" json:"name"`
 
 	Members []Member `gorm:"foreignKey:PositionID;constraint:OnDelete:CASCADE" json:"-"`
 
